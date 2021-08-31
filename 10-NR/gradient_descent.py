ecf8ef88af6ee96ceb5123ec76c88e434ad6d678
unknown <50898990+1155107756@users.noreply.github.com>
2021-08-31 18:44:47 +0800

    Update gradient_descent.py

--- 10-NR/gradient_descent.py
+++ 10-NR/gradient_descent.py
@@ -12,33 +12,63 @@ from guess_z import predict_z
 
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
-# A and b are N * 2 size tenors out put the sum of distances of each pairs
-def distance(A):
-  dis = tf.reduce_sum(
-    # tf.sqrt(tf.maximum(
-    tf.reduce_sum((tf.expand_dims(A, 1)-tf.expand_dims(A, 0))**2, 2)
-    #, 1e-10 ))
-  )
-
+# A is an T * N * 3 size tenor out put the sum of distances of each pairs
+# output T * N * N
+def distance(A: tf.Tensor):
+  # A.shape = (T, N, 3)
+  A1 = tf.expand_dims(A, 2)
+  A2 = tf.expand_dims(A, 1)
+  dis = tf.norm(A1 - A2 ,axis=-1)
   return dis
+  # dis.shape = (T, N, N)
 
-def loss_function(points, num_frames):
-  loss = 0
-  for t in range(0, num_frames - 1):
-    loss += tf.math.abs(distance(points[t]) - distance(points[t+1]))
-                #0.1 * tf.norm(z[t] - z[t + 1])
-  return loss
+# points: T * N * 3 size tensor
+def loss_function(points):
+  # distances: T * N * N
+  D = distance(points)
+  # pair_dif = T * T * N * N
+  pair_dif = tf.abs((tf.expand_dims(D, 1) - tf.expand_dims(D,0)))
+  output = tf.reduce_sum(pair_dif)
+  return output
 
-def loss_function_evaluate(points, num_frames):
-  loss = 0
-  for t in range(0, num_frames - 2):
-    loss += tf.math.abs(
-      distance(tf.slice(points, [t, 0, 0], [t,-1,-1]),tf.slice(points, [t+1, 0, 0], [t+1,-1,-1])))
-  return loss
+def training(points):
+  sess = tf.Session()
+  loss = loss_function(points)
+
+  # Construct an optimizer
+  optimizer = tf.train.GradientDescentOptimizer(1e-6)
+  train_step = optimizer.minimize(loss)
+
+  # Initialize w in GPU
+  sess.run(tf.global_variables_initializer())
+  print('[0] loss = {}'.format(sess.run(loss)))
+  print(sess.run(z))
+
+  pre_value_loss = 99999999
+  for t in range(99999999):
+    sess.run(train_step)
+    val_loss = sess.run(loss)
+
+    # if val_loss < 30:
+    # if t % 10000 == 0 and np.abs(pre_value_loss - val_loss) < 0.01:
+    if val_loss < 500:
+      print(f'Training compeleted at step {t} with loss:{val_loss}')
+      print('z = {}'.format(sess.run(z)))
+      with open('./z.json', 'w') as f:
+        json.dump(sess.run(z).tolist(), f)
+      with open('./x.json', 'w') as f:
+        json.dump(sess.run(x).tolist(), f)
+      with open('./y.json', 'w') as f:
+        json.dump(sess.run(y).tolist(), f)
+      break
+    if t % 10000 == 0:
+      print('[{}] loss = {}'.format(t + 1, val_loss))
+      print('z = {}'.format(sess.run(z)))
+      pre_value_loss = val_loss
 
 if __name__ == '__main__':
-  max_particle = 20
-  max_frame = 10
+  max_particle = 100
+  max_frame = 100
   #x, y = read_x_y_from_pkl('data_3.pkl', max_particle=20, max_frame=3
   x = json.load(open('./ground_true/x.json'))
   y = json.load(open('./ground_true/y.json'))
@@ -63,13 +93,12 @@ if __name__ == '__main__':
   y = tf.constant(y, dtype=tf.float64)
 
   points = tf.transpose([x, y, z], perm=[1, 2, 0])
-  print(points)
   # Initialize a session
   sess = tf.Session()
-  loss = loss_function(points,num_frames)
+  loss = loss_function(points)
 
   # Construct an optimizer
-  optimizer = tf.train.GradientDescentOptimizer(0.00000001)
+  optimizer = tf.train.GradientDescentOptimizer(1e-6)
   train_step = optimizer.minimize(loss)
 
   # Initialize w in GPU
@@ -84,8 +113,8 @@ if __name__ == '__main__':
 
     #if val_loss < 30:
     #if t % 10000 == 0 and np.abs(pre_value_loss - val_loss) < 0.01:
-    if  val_loss < 0.01:
-      print(val_loss)
+    if  val_loss < 500:
+      print(f'Training compeleted at step {t} with loss:{val_loss}')
       print('z = {}'.format(sess.run(z)))
       with open('./z.json', 'w') as f:
         json.dump(sess.run(z).tolist(), f)
