f53ee4f919a9d2248d4ea6570cc01ac044969eeb
unknown <50898990+1155107756@users.noreply.github.com>
2021-09-07 20:24:48 +0800

    Update visualizations

--- 10-NR/visualize_test_xyz.py
+++ 10-NR/visualize_test_xyz.py
@@ -1,7 +1,9 @@
+import json
+
 from visualize_xyz import visualize_xyz
 if __name__ == '__main__':
   vis_2 = visualize_xyz()
   f = open('train_result.json', 'rb')
-  x, y, z = vis_2.read_json(f)
-  vis_2.vis_animation(x, y, z)
+  vis_2.read_x_y_z_json(json.load(f))
+  vis_2.add_plotter(vis_2.draw_3d)
   vis_2.show()
--- 10-NR/visualize_xyz.py
+++ 10-NR/visualize_xyz.py
@@ -8,68 +8,47 @@ class visualize_xyz(DaVinci):
   def __init__(self, size: int = 5, **kwargs):
     # Call parent's constructor
     super(visualize_xyz, self).__init__('Nuclei Visualizer', height=size, width=size)
+    self.keep_3D_view_angle = True
 
-  def draw_xyz(self, xlist=None, ylist=None, zlist=None):
-    fig = self.figure
-    ax = self.axes3d
-    self.axes3d.view_init(elev=30, azim=0)
-    #ax.set_axis_off()
-    #fig.set_facecolor('black')
-    #ax.set_facecolor('black')
-    #ax.grid(False)
-    #ax.w_xaxis.pane.fill = False
-    #ax.w_yaxis.pane.fill = False
-    #ax.w_zaxis.pane.fill = False
-    z = np.array(zlist)
-    xlist, ylist, zlist = np.array(xlist),np.array(ylist), np.array(zlist)
-    ax.plot3D(xlist[z>0],ylist[z>0],zlist[z>0], 'ro', markersize=2)
-    ax.plot3D(xlist[z < 0], ylist[z < 0], zlist[z < 0], 'bo', markersize=2)
-
-  def vis_animation(self,x,y,z):
-    for i in range(len(x)):
-      def visual(x_=x[i], y_=y[i], z_=z[i]):
-        self.draw_xyz(xlist=x_, ylist=y_, zlist=z_)
-      self.add_plotter(visual)
-
-  def read(self,path):
-    f = open(path, 'rb')
-    df: pd.DataFrame = pickle.load(f)
-    x, y, z = [], [], []
+  def read_x_y_data(self, df):
+    self.objects = []
     for f in range(int(df['frame'].max()) + 1):
-      x.append(df[df['frame'] == f]['x'].tolist())
-      y.append(df[df['frame'] == f]['y'].tolist())
-      z.append(df[df['frame'] == f]['z'].tolist())
-    return x,y,z
+      self.objects.append([df[df['frame'] == f]['x'].tolist(),
+                           df[df['frame'] == f]['y'].tolist()])
 
-  def read_df(self, df):
-    x, y, z = [], [], []
+  def read_x_y_z_data(self, df):
+    self.objects = []
     for f in range(int(df['frame'].max()) + 1):
-      x.append(df[df['frame'] == f]['x'].tolist())
-      y.append(df[df['frame'] == f]['y'].tolist())
-      z.append(df[df['frame'] == f]['z'].tolist())
-    return x, y, z
+      self.objects.append([df[df['frame'] == f]['x'].tolist(),
+                           df[df['frame'] == f]['y'].tolist(),
+                           df[df['frame'] == f]['z'].tolist()])
 
-  def read_json(self, js):
-    data = np.transpose(np.array(json.load(js)) ,axes=[2,0,1])
-    x = data[0]
-    y = data[1]
-    z = data[2]
-    return x, y, z
+  def read_x_y_z_json(self, data):
+    self.objects = np.transpose(np.array(data), axes=[0,2,1]).tolist()
+
+  def draw_2d(self, x, ax):
+    ax.set_xlim(-1.2, 1.2)
+    ax.set_ylim(-1.2, 1.2)
+    ax.scatter(*x, s=5)
+
+  def draw_3d(self, x, ax3d, divide_pos_neg = True):
+    if divide_pos_neg:
+      x_, y_, z_ = x
+      z = np.array(z_)
+      xlist, ylist, zlist = np.array(x_), np.array(y_), np.array(z_)
+      ax3d.plot3D(xlist[z > 0], ylist[z > 0], zlist[z > 0], 'ro', markersize=2)
+      ax3d.plot3D(xlist[z < 0], ylist[z < 0], zlist[z < 0], 'bo', markersize=2)
+    else:
+      ax3d.scatter(*x, 'bo', s=5)
+
+
 
 
 if __name__ == '__main__':
+  f = open('data.pkl', 'rb')
+  df: pd.DataFrame = pickle.load(f)
+  df = df[df['z'] > 0]
   vis = visualize_xyz()
-  x_g, y_g, z_g = vis.read('data.pkl')
-  max_particle = 100
-  max_frame = 100
-  x_g = np.array(x_g)[0:max_frame, 0:max_particle].tolist()
-  y_g = np.array(y_g)[0:max_frame, 0:max_particle].tolist()
-  z_g = (np.array(z_g)[0:max_frame, 0:max_particle]).tolist()
-  print(x_g,y_g,z_g)
-  vis.vis_animation(x_g, y_g, z_g)
-  '''def visualzie():
-    vis.axes3d.view_init(elev=90, azim=0)
-    vis.axes3d.plot3D(x_g[0], y_g[0], z_g[0], 'ro', markersize=2)
-  vis.add_plotter(visualzie)'''
+  vis.read_x_y_data(df)
+  vis.add_plotter(vis.draw_2d)
   vis.show()
-  #vis.add_plotter(visualize_ground_true)
